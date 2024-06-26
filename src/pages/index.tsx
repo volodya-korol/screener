fcb8f74954b2ed568b9a34e084caae098b4eae5f
volodya-k <volodymyr.korol@izzi.software>
2024-06-26 23:00:24 +0300

    123

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,7 +18,7 @@ export const getServerSideProps = async () => {
 				"content-type": "application/json;charset=UTF-8",
 				"accept-language": "*",
 				"sec-fetch-mode": "cors",
-				"user-agent": "undici",
+				// "user-agent": "undici",
 				"accept-encoding": "gzip, deflate",
 				"content-length": "17",
 			},
@@ -31,7 +31,7 @@ export const getServerSideProps = async () => {
 		// @ts-ignore
 		console.log("====================================", error?.response?.status);
 		// @ts-ignore
-		console.log({  config: error?.config, request: error?.request });
+		console.log({  config: error?.config, request: error?.request?.headersList });
 		console.log("================erre====================");
 	}
 
